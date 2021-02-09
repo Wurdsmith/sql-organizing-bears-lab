4c40850356421bbe4a27890ae77256fa49268d58
Wurdsmith <wurdsmithing@gmail.com>
2021-02-09 10:49:58 -0600

    commit tree 4dac5e0ba48dfd83124e4fc80b8850aeb3a42660

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Mr. Chocolate", 15, "M", "Brownish", "Sleepy", 1),
+("Rowdy", 21, "M", "Brown", "Growchy", 1),
+("Tabitha", 24, "F", "White", "Friendly", 0),
+("Sergeant Brown", 6, "M", "Brown", "Sassy", 1),
+("Melissa", 2, "F", "Dark brown", "Silly", 1),
+("Grinch", 8, "M", "Brown", "Surly", 1),
+("Wendy", 18, "F", "Brown", "Nosey", 0),
+(null, 16, "M", "Brown", "Hungry", 1);
